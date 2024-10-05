3950cfa0461e16ebaa96bda43510cced83a17adc
league <league@imac-18.attlocal.net>
2024-10-04 18:27:21 -0700

    Finished Lambdas, working on Streams

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,7 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,13 +15,46 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String output = "";
+			for(int i = s.length() - 1; i >= 0; i--)
+			{
+				output += s.charAt(i);
+			}
+			System.out.println(output);
+		}, "reverse");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			Random rand = new Random();
+			String output = "";
+			for(int i = 0; i < s.length(); i++)
+			{
+				if (rand.nextBoolean())
+				{
+					output += Character.toUpperCase(s.charAt(i));
+				}
+				else
+				{
+					output += Character.toLowerCase(s.charAt(i));
+				}
+			}
+			System.out.println(output);
+		}, "Mixed Cases");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String output = "";
+			for (int i = 0; i < s.length(); i++)
+			{
+				output += s.charAt(i) + (i < s.length() - 1 ? "." : "");
+			}
+			System.out.println(output);
+		}, "Period between each character");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String regex = "[aeiou]";
+			s = s.replaceAll(regex, "");
+			System.out.println(s);
+		}, "can you remove all vowels from a string");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
